ec8134a1dafcf3525f270cdd0bc925b4f2de57c5
Rohit Patil <71687498+falconcode16@users.noreply.github.com>
2021-01-27 19:45:03 +0530

    Add files via upload

---
+++ To check phone affordibility.c
@@ -0,0 +1,17 @@
+
+
+#include<stdio.h>
+
+main()
+{
+    int phone;
+
+printf("enter the price of phone\n");
+scanf("%d",&phone);
+
+
+    if(phone==10000)
+    {
+        printf("phone can be puchased\n");
+    }
+}

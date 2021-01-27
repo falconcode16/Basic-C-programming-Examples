7eb3566f9c6013ed4f94e7730e4b6bfa56c12887
Rohit Patil <71687498+falconcode16@users.noreply.github.com>
2021-01-27 19:07:51 +0530

    Add files via upload

---
+++ To calculate Simple Interest.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+main()
+{
+ float p,t,r,si;
+ printf("enter the of value p, t ,r");
+
+ scanf("%f%f%f",&p,&t,&r);
+ si=(p*r*t)/100;
+ printf("si is =%f\n",si);
+
+
+}
